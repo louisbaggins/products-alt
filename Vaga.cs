fc6612e666adc524959a46c13199f3cf891c6eec
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-12-27 13:48:27 -0200

    Add files via upload

---
+++ Vaga.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlWrk.Models
+{
+   
+       public class Vaga
+        {
+            public string Cargo { get; set; }
+            public string Link { get; set; }
+            public Vaga()
+            {
+            }
+            public Vaga(string Cargo, string link)
+            {
+                this.Cargo = Cargo;
+                this.Link = link;
+            }
+
+        }
+    }
